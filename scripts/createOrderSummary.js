93a74246a95e41b74acdf5342fdccfcbca33b4ba
DragostinH <dragostin.hristov@gmail.com>
2022-05-09 21:34:27 +0300

    feat: added home develiry and collect options on the summary screen.

--- scripts/createOrderSummary.js
+++ scripts/createOrderSummary.js
@@ -1,27 +1,30 @@
-import createArticle from "./createElementsFunctions/createArticle.js";
 import createBtn from "./createElementsFunctions/createBtn.js";
 import createDiv from "./createElementsFunctions/createDiv.js";
 import createParagraph from "./createElementsFunctions/createParagraph.js";
 
+const totalValueElement = document.querySelector('.total-value');
+const parsedStorage = JSON.parse(localStorage.shoppingCart);
+const orderItemsContainer = document.querySelector('.order-items-container');
+
 
 const createOrderSummary = (() => {
-    const orderItemsContainer = document.querySelector('.order-items-container');
-    const totalValueElement = document.querySelector('.total-value');
     let total = 0;
-    const parsedStorage = JSON.parse(localStorage.shoppingCart);
 
     for (const key in parsedStorage) {
         if (Object.hasOwnProperty.call(parsedStorage, key)) {
             const element = parsedStorage[key];
-            orderItemsContainer.appendChild(createOrderSummaryElement(element.qty, element.name, element.price, orderItemsContainer));
+            orderItemsContainer.appendChild(
+                createOrderSummaryElement(
+                    element.qty,
+                    element.name,
+                    element.price,
+                    orderItemsContainer));
             total += (element.price * element.qty);
         }
     }
-    
     totalValueElement.textContent += `$${total.toFixed(2)}`;
 
 })();
-
 
 function createOrderSummaryElement(qty, name, price, main) {
     const orderItemDiv = createDiv("order-item");
@@ -37,19 +40,17 @@ function createOrderSummaryElement(qty, name, price, main) {
     removeItem.textContent = "Remove";
 
     removeItem.addEventListener('click', () => {
-        const parsedStorage = JSON.parse(localStorage.shoppingCart);
-        if (name in parsedStorage) {
-            if (parsedStorage[name].qty > 1) {
-                parsedStorage[name].qty--;
-            } else if (parsedStorage[name].qty === 1) {
-                delete parsedStorage[name];
-                main.removeChild(orderItemDiv)
-            }
+        if (parsedStorage[name].qty > 1) {
+            parsedStorage[name].qty--;
+            refreshSummaryPage();
+        } else if (parsedStorage[name].qty === 1) {
+            delete parsedStorage[name];
+            main.removeChild(orderItemDiv);
         }
-        calculateTotal(price)
+        calculateTotal(price);
         localStorage.shoppingCart = JSON.stringify(parsedStorage);
+    });
 
-    })
     orderItemDiv.appendChild(orderItemLeft);
     orderItemDiv.appendChild(orderItemRight);
     orderItemLeft.appendChild(qtyP);
@@ -58,11 +59,27 @@ function createOrderSummaryElement(qty, name, price, main) {
     orderItemRight.appendChild(removeItem);
 
     return orderItemDiv;
+};
 
-}
 function calculateTotal(amountToSubtract) {
-    const totalEl = document.querySelector('.total-value');
-    let currTotal = totalEl.textContent.split(" ")[1].split("$")[1] * 1;
-    let newTotal = currTotal - amountToSubtract
-    totalEl.textContent = `Total: $${newTotal.toFixed(2)}`
+    let currTotal = totalValueElement.textContent.split(" ")[1].split("$")[1] * 1;
+    let newTotal = currTotal - amountToSubtract;
+    totalValueElement.textContent = `Total: $${newTotal.toFixed(2)}`;
 }
+
+function refreshSummaryPage() {
+    while (orderItemsContainer.hasChildNodes()) {
+        orderItemsContainer.removeChild(orderItemsContainer.firstChild);
+    }
+    for (const key in parsedStorage) {
+        const element = parsedStorage[key];
+        const orderItem = createOrderSummaryElement(
+            element.qty,
+            element.name,
+            element.price,
+            orderItemsContainer);
+        orderItemsContainer.appendChild(orderItem);
+
+    }
+}
+
